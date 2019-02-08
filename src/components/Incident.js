fb12e3ee66498d9765f66aca6d8c7d944d56d194
Bence Varga <vbence86@gmail.com>
2019-02-08 18:06:48 +0100

    Create data model to wrap raw incident data

---
+++ src/components/Incident.js
@@ -0,0 +1,44 @@
+class Incident {
+  constructor(json) {
+    this.data = json || {};
+  }
+
+  getName() {
+    const { common_place_name } = this.getAddress();
+    return common_place_name;
+  }
+
+  getAddressString() {
+    const { address_line1, city, state } = this.getAddress();
+    return `${address_line1}, ${city}, ${state}`;
+  }
+
+  getAddress() {
+    return this.data.address;
+  }
+
+  getLatLong() {
+    const { latitude, longitude } = this.getAddress();
+    return {
+      lat: latitude,
+      lng: longitude,
+    };
+  }
+
+  getDescription() {
+    const { comments } = this.data.description;
+  }
+
+  getOpenTime() {
+    const { event_opened } = this.data.description;
+    return event_opened;
+  }
+
+  getSubType() {
+    const { subtype } = this.data.description;
+    return subtype;
+  }
+}
+
+export { Incident };
+export default Incident;
