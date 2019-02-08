d3107d5123e7db9dc79ad17f893430146200cdcd
Bence Varga <vbence86@gmail.com>
2019-02-08 12:35:29 +0100

    remove build directory from repository

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d9bb8109cf9447aedc263459a8859c16.js"
+  "/prominent-edge-test-app/precache-manifest.2522dba43f5400a0aef206ac597aa004.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/prominent-edge-test-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
